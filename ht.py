e04a56bf849818907be4730f1b486b56d0f59ddd
Ariana Treat <aritreat@sas.upenn.edu>
2024-05-21 17:46:29 -0400

    HW1 troubleshooting pt9

--- ht.py
+++ ht.py
@@ -3,5 +3,5 @@ import hello
 def test_hello():
     assert hello.hello_world(1) == "Hello World!"
     
-def test_Nhello():
-    assert hello.hello_world_n(3) == "Hello World! Hello World! Hello World!"
+def hello_world_n(n):
+    return " ".join(["Hello World!"] * n)
