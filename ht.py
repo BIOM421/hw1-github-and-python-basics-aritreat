ad443168311c6033786429d2cdcafae002e9fe84
Ariana Treat <aritreat@sas.upenn.edu>
2024-05-21 21:03:45 -0400

    fixing ht.py

--- ht.py
+++ ht.py
@@ -1,7 +1,9 @@
-import hello
+def hello_world():
+	return "Hello World!"
+result = hello_world()
+print(result)
 
-def test_hello():
-    assert hello.hello_world(1) == "Hello World!"
-    
-def hello_world_n(n):
-    return " ".join(["Hello World!"] * n)
+def hello_world_n(N):
+	N = int(N)
+	result_1 =  "Hello World!" * N
+	return result_1	
